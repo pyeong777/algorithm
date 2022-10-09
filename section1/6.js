d65d120f7337ec71684cfd0e7ff5e706e7c6a054
pyeong777 <sdp1123@naver.com>
2022-10-09 23:33:23 +0900

    ✅ 홀수배열

--- section1/6.js
+++ section1/6.js
@@ -1,11 +1,15 @@
 const solution = (a, b, c, d, e, f, g) => {
+    let answer = [];
     let arr = [a, b, c, d, e, f, g];
     let sum = 0;
+
     let oddArr = arr.filter((it) => it % 2 !== 0);
     oddArr.forEach((it) => sum += it);
+
     let minValue = Math.min(...oddArr);
 
-    answer = `${sum} ${minValue}`
+    answer.push(sum);
+    answer.push(minValue);
 
     return answer;
 };
