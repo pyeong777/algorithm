c602a6d65c1de006c0fd129f8df6f2c4467a8795
pyeong777 <sdp1123@naver.com>
2022-10-14 21:55:20 +0900

    ⚡️ reduce 함수

--- 1, 2차원 배열 탐색/1.js
+++ 1, 2차원 배열 탐색/1.js
@@ -1,9 +1,17 @@
 const solution = (arr) => {
+    // let answer = [];
+    // let arr2 = arr.filter((v, i) => v > arr[i - 1]);
+    // answer.push(arr[0], ...arr2)
+
+    // return answer;
     let answer = [];
-    let arr2 = arr.filter((v, i) => v > arr[i - 1]);
-    answer.push(arr[0], ...arr2)
+    arr.reduce((prev, cur) => {
+        if (prev < cur) answer.push(cur);
+        return cur;
+    }, 0)
 
     return answer;
+
 };
 
 const arr = [7, 3, 9, 5, 6, 12]
