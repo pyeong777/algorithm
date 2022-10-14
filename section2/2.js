052681077fbf83766eed1297847a1473579fa93c
pyeong777 <sdp1123@naver.com>
2022-10-14 22:38:13 +0900

    ✅ 보이는 학생

---
+++ section2/2.js
@@ -0,0 +1,15 @@
+const solution = (arr) => {
+    let answer = [];
+    let max = arr[0];
+    answer.push(arr[0]);
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            answer.push(arr[i]);
+            max = arr[i];
+        }
+    }
+    return answer.length;
+};
+
+const arr = [130, 135, 148, 140, 145, 150, 150, 153];
+console.log(solution(arr));
